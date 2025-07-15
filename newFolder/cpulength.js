405bade0f87829c4c0163801254aff0d7622708f
KARAN SINGH PAL <118987213+Karansinghpal@users.noreply.github.com>
2025-07-15 18:50:34 +0000

    config

---
+++ newFolder/cpulength.js
@@ -0,0 +1,3 @@
+const os = require('os');
+console.log(os.cpus());// Get information about the CPU
+console.log(os.cpus().length); // Get the number of CPU cores
--- newFolder/file.js
+++ newFolder/file.js
@@ -2,6 +2,7 @@ const fs = require('fs');
 
 
 
+
 /////////This Is the Synchronous Methods///////////
 // fs.writeFileSync('./text.txt', "Hii Karan Text File Created Succesfully")
 
@@ -12,13 +13,13 @@ const fs = require('fs');
 
 
 ////////////This Is the Asynchronous Methods////////
-fs.readFile('./read.txt', 'utf-8', (err, data) => {
-  if (err) {
-    console.error('Error reading file:', err);
-  } else {
-    console.log('File content:', data);
-  }
-});
+// fs.readFile('./read.txt', 'utf-8', (err, data) => {
+//   if (err) {
+//     console.error('Error reading file:', err);
+//   } else {
+//     console.log('File content:', data);
+//   }
+// });
 
 
 
@@ -33,5 +34,15 @@ fs.readFile('./read.txt', 'utf-8', (err, data) => {
 
 //////////This is Append Method in your Txt////////
 //  fs.appendFileSync('./read.txt', 'This is appended text\n'); // Appending text to the file
+
+
+// console.log(fs.statSync('./read.txt'));//show u stats of file like size, birthtime, etc.
+
+
+
+// fs.cpSync('./read.txt', './Copy.txt     '); // Copying file to new folder
+
+// fs.unlinkSync('./read.txt'); // Deleting file
+
 
 
