973cae458f459d0d7de917d1ed74766d5cdff80d
KARAN SINGH PAL <118987213+Karansinghpal@users.noreply.github.com>
2025-07-15 18:20:40 +0000

    config

--- newFolder/file.js
+++ newFolder/file.js
@@ -1,7 +1,37 @@
 const fs = require('fs');
 
+
+
+/////////This Is the Synchronous Methods///////////
 // fs.writeFileSync('./text.txt', "Hii Karan Text File Created Succesfully")
 
-const result = fs.readFileSync('./read.txt', 'utf-8')
+// const result = fs.readFileSync('./read.txt', 'utf-8')
 
-console.log(result);
+// console.log(result);
+
+
+
+////////////This Is the Asynchronous Methods////////
+fs.readFile('./read.txt', 'utf-8', (err, data) => {
+  if (err) {
+    console.error('Error reading file:', err);
+  } else {
+    console.log('File content:', data);
+  }
+});
+
+
+
+
+///////This is Date Append Method in your Txt////////
+// const hi =  fs.appendFileSync('./read.txt', new Date().getDate().toLocaleString());
+// console.log(hi,);
+
+
+
+
+
+//////////This is Append Method in your Txt////////
+//  fs.appendFileSync('./read.txt', 'This is appended text\n'); // Appending text to the file
+
+
